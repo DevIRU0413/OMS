c8470d96fc183763f0ad1a00b6f63f9216d35738
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-30 02:23:34 +0900

    [feat]: 플레이어 데이터용 SO 로직 추가
    - 플레이어 데이터 컨테이너용 SO 추가
    - Action OnCurrentHPChanged을 통해 UI 연동 가능

---
+++ Assets/00_WorkSpace/MSG/Player/MSG_PlayerData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace MSG
+{
+    // Model을 SO로 해도 되나?
+    [CreateAssetMenu(fileName = "MSG_PlayerData", menuName = "ScriptableObjects/MSG_PlayerData")]
+    public class MSG_PlayerData : ScriptableObject
+    {
+        public const int MaxHP = 100;
+        public const int MinHP = 0;
+
+        [SerializeField] private int _startHP;
+        public int CurrentHP { get; private set; }
+        [field: SerializeField] public float RunSpeed { get; private set; }
+        [field: SerializeField] public float WalkMoveSpeed { get; private set; }
+        [field: SerializeField] public float DebuffedMoveSpeed { get; private set; }
+        [field: SerializeField] public float HPDecreasePerSecond { get; private set; }
+
+        public event Action<int> OnCurrentHPChanged;
+
+        public void Init()
+        {
+            CurrentHP = _startHP;
+            OnCurrentHPChanged?.Invoke(CurrentHP);
+        }
+    }
+}
