389e3ed6738080264dc75077fc577701b1d58323
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-30 02:24:15 +0900

    [feat]: 플레이어 로직용 클래스 생성
    - 아직 클래스 생성 및 필드만 설정하고 미구현 상태

---
+++ Assets/00_WorkSpace/MSG/Player/MSG_PlayerLogic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class MSG_PlayerLogic : MonoBehaviour
+    {
+        [SerializeField] private MSG_PlayerData _playerData;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private CapsuleCollider2D _capsuleCollider2D;
+
+        private bool _isWornBreadBag = false;
+        private bool _isFever = false;
+
+        public MSG_PlayerData PlayerData => _playerData;
+        public bool IsFever => _isFever;
+
+        public event Action OnPlayerFeverStarted;
+        public event Action OnPlayerFeverEnded;
+        public event Action OnPlayerDied;
+
+
+        private void Awake()
+        {
+            _playerData.Init();
+        }
+
+        private void Update()
+        {
+            LowerHPByTime();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            // 아이템 처리
+            // 장애물 NPC 부딪힘 처리
+        }
+
+        private void LowerHPByTime()
+        {
+
+        }
+    }
+}
