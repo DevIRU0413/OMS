8eeefff0794f656832136bd5c1911b5c32f1d549
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-30 02:25:13 +0900

    [feat]: 마우스를 통한 조작을 지원하는 로직 추가
    - 플레이어 기준으로 마우스 좌우 상태를 판단하여 이동시키는 로직 추가
    - 추후 Raycast로 NPC 포획 시스템 추가 예정

---
+++ Assets/00_WorkSpace/MSG/Player/MSG_MouseCursorManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+
+namespace MSG
+{
+    public class MSG_MouseCursorManager : MonoBehaviour
+    {
+        private MSG_PlayerLogic _playerLogic;
+        private MSG_PlayerData _playerData;
+
+        private void Start()
+        {
+            _playerLogic = MSG_PlayerReferenceProvider.Instance.GetPlayerLogic();
+            _playerData = MSG_PlayerReferenceProvider.Instance.GetPlayerData();
+        }
+
+        private void Update()
+        {
+            MoveByMousePos();
+        }
+
+        private void MoveByMousePos()
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 playerPos = _playerLogic.transform.position;
+
+            float deltaX = mouseWorldPos.x - playerPos.x;
+            float absDelta = Mathf.Abs(deltaX);
+
+            float speed = 0f;
+            if (absDelta < 0.5f)
+            {
+                speed = 0f;
+            }
+            else if (absDelta < 2f)
+            {
+                speed = _playerData.WalkMoveSpeed;
+            }
+            else
+            {
+                speed = _playerData.RunSpeed;
+            }
+
+            float moveDir = Mathf.Sign(deltaX);
+            Vector3 move = new Vector3(moveDir * speed, 0f, 0f);
+            _playerLogic.transform.position += move * Time.deltaTime;
+        }
+
+        // TODO: Raycast로 ICollectible 인터페이스인 경우 MoveByMousePos() 멈추고 클릭 시 포획 시도
+        // 만약 해당 NPC가 경쟁 중인 상태라면 클릭 연타로 변경
+    }
+}
