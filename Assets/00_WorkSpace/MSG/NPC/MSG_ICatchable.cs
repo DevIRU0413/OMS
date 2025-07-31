690886df312d7cf6ba2265595b11816018cced2c
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 01:55:32 +0900

    [feat]: ICatchable 인터페이스 추가
    - 포획가능 NPC의 행동을 관리하기 위한 인터페이스 추가

---
+++ Assets/00_WorkSpace/MSG/NPC/MSG_ICatchable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public interface MSG_ICatchable
+    {
+        public void OnCatchPressed();  // 마우스를 눌렀을 때
+        public void OnCatchReleased(); // 마우스를 떼었을 때
+        public void OnHoverEnter();    // 마우스가 대상 위에 올라갔을 때
+        public void OnHoverExit();     // 마우스가 대상에서 벗어났을 때
+    }
+}
