f0174e77d094f6c7e5527f1c28d55b0c816c1281
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 02:07:57 +0900

    [feat]: 포획 가능 NPC의 경쟁 상태 추가
    - 경쟁 중일 때 주변 라이벌의 힐 총량 만큼 게이지가 줄어드는 로직 구현
    - 경쟁 중일 때는 마우스를 꾹 누르는 것이 아닌 마우스를 클릭할 때마다 게이지가 오르도록 구현

---
+++ Assets/00_WorkSpace/MSG/NPC/State Machine/MSG_CompetingState.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class MSG_CompetingState : MSG_INpcState
+    {
+        private MSG_CatchableNPC _npc;
+        public MSG_CompetingState(MSG_CatchableNPC npc)
+        {
+            _npc = npc;
+        }
+
+        public void Enter()
+        {
+            _npc.StartCompete();
+            _npc.StopMovement(true);
+        }
+
+
+        public void Update()
+        {
+            _npc.DecreaseGauge(Time.deltaTime * _npc.TotalHealPointPerSecond);
+
+            if (_npc.IsGaugeEmpty())
+            {
+                _npc.ChangeState(new MSG_CatchingState(_npc));
+            }
+            else if (_npc.IsGaugeFull())
+            {
+                _npc.ChangeState(new MSG_CapturedState(_npc));
+            }
+        }
+
+        public void Exit()
+        {
+            _npc.EndCompete();
+            _npc.StopMovement(false);
+        }
+        public void OnCatchPressed() 
+        {
+            _npc.IncreaseGauge(_npc.Settings.CaptureGaugeIncreasePerClick); 
+        }
+
+        #region Unused Methods
+
+        public void OnCatchReleased() { }
+        public void OnHoverEnter() { }
+        public void OnHoverExit() { }
+        #endregion
+    }
+}
