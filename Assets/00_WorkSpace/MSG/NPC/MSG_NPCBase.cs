8bbba2ae69814ebce86d6390a9077fa394e8820e
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 01:47:00 +0900

    [feat]: NPC 추상 클래스 추가
    - 모든 NPC가 갖는 공통 기능을 정의하기 위한 추상 클래스 추가
    - NPC데이터, 애니메이터, 콜라이터, 스프라이트렌더러 등을 필드로 포함
    - MSG_NPCDataSO를 Public으로 열어 UI 및 외부에서 사용 가능하도록 함
    - MSG_NPCDataSO는 현재 CSV 파싱 기능 개발 전 개발용으로 사용 중

---
+++ Assets/00_WorkSpace/MSG/NPC/MSG_NPCBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public abstract class MSG_NPCBase : MonoBehaviour
+    {
+        [SerializeField] protected MSG_NPCDataSO _npcData;
+        [SerializeField] protected Animator _animator;
+        [SerializeField] protected CapsuleCollider2D _collider;
+        [SerializeField] protected SpriteRenderer _spriteRenderer;
+
+        // CSV 파싱 기능 개발 전 NPC 작동을 위한 SO 주입
+        public MSG_NPCDataSO NPCData => _npcData;
+
+        protected virtual void OnEnable()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _npcData.Init();
+        }
+    }
+}
