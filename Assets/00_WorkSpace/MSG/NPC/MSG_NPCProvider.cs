348fa551f40c6c046fd7ec4ac98f2496c31505f4
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 02:03:34 +0900

    [feat]: NPC를 반환하는 GetComponent 우회용 헬퍼 클래스 추가
    - NPC의 콜라이더를 key로 하여 Raycast에서 얻은 콜라이더를 통해 NPC를 받을 수 있는 구조

---
+++ Assets/00_WorkSpace/MSG/NPC/MSG_NPCProvider.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public static class MSG_NPCProvider
+    {
+        private static Dictionary<Collider2D, MSG_RivalNPC> _rivalNpcMap = new();
+        private static Dictionary<Collider2D, MSG_CatchableNPC> _catchableNpcMap = new();
+
+
+        public static void RegisterRival(MSG_RivalNPC rival, CapsuleCollider2D col)
+        {
+            _rivalNpcMap[col] = rival;
+        }
+
+        public static void UnregisterRival(MSG_RivalNPC rival, CapsuleCollider2D col)
+        {
+            _rivalNpcMap.Remove(col);
+        }
+
+        public static bool TryGetRival(CapsuleCollider2D col, out MSG_RivalNPC npc)
+        {
+            return _rivalNpcMap.TryGetValue(col, out npc);
+        }
+
+
+        public static void RegisterCatchable(MSG_CatchableNPC catchable, Collider2D col)
+        {
+            _catchableNpcMap[col] = catchable;
+        }
+        public static void UnregisterCatchable(MSG_CatchableNPC catchable, Collider2D col)
+        {
+            _catchableNpcMap.Remove(col);
+        }
+        public static bool TryGetCatchable(Collider2D col, out MSG_CatchableNPC npc)
+        {
+            return _catchableNpcMap.TryGetValue(col, out npc);
+        }
+
+        public static void Clear()
+        {
+            _rivalNpcMap.Clear();
+            _catchableNpcMap.Clear();
+        }
+    }
+}
