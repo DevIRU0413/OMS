d6ed51fb186bdfb0977c5ca626009d438bdf27c8
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 02:03:51 +0900

    [feat]: 라이벌 NPC 클래스 추가
    - 현재 미구현 상태

---
+++ Assets/00_WorkSpace/MSG/NPC/MSG_RivalNPC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class MSG_RivalNPC : MSG_NPCBase
+    {
+        private void Start()
+        {
+            MSG_NPCProvider.RegisterRival(this, _collider);
+
+        }
+
+        private void OnDestroy()
+        {
+            MSG_NPCProvider.UnregisterRival(this, _collider);
+        }
+
+    }
+}
