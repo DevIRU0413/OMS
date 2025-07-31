c29c149202aab01d6b9b37ca86294ad988b0755a
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 02:08:47 +0900

    [feat]: 포획 가능 NPC의 포획 상태 추가
    - 포획된 NPC의 상태 추가 및 상호작용 비활성화 처리
    - 추후 플레이어를 따라다니는 로직 구현 예정

---
+++ Assets/00_WorkSpace/MSG/NPC/State Machine/MSG_CapturedState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSG
+{
+    public class MSG_CapturedState : MSG_INpcState
+    {
+        private MSG_CatchableNPC _npc;
+
+        public MSG_CapturedState(MSG_CatchableNPC npc)
+        {
+            _npc = npc;
+        }
+
+        public void Enter()
+        {
+            _npc.MarkAsCaptured();
+            _npc.PlayCaptureEffect();
+            _npc.DisableInteraction();
+        }
+        public void Update()
+        {
+            // 플레이어 뒤를 따라가게
+        }
+
+
+        #region Unused Methods
+        public void Exit() { }
+        public void OnHoverEnter() { }
+        public void OnHoverExit() { }
+        public void OnCatchPressed() { }
+        public void OnCatchReleased() { }
+        #endregion
+    }
+}
