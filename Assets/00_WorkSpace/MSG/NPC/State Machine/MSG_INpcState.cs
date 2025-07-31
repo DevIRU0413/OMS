089ff4e93b8fe79e9f4dce5d4369f5a80034d8db
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 02:04:34 +0900

    [feat]: NPC 상태 인터페이스 추가
    - 포획 가능 NPC의 상태 분기가 너무 많아 상태패턴으로 처리

---
+++ Assets/00_WorkSpace/MSG/NPC/State Machine/MSG_INpcState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    /*
+    상태 목록
+    
+    Wandering:      배회 상태
+    Aimed:          마우스가 올라간 상태
+    Catching:       경쟁 중이지 않은, NPC를 잡는 상태
+    Competing:      경쟁 중인 상태
+    Captured:       잡힌 상태
+    CaptureFailed:  잡기에 실패하여 사라진 상태
+
+     */
+
+    public interface MSG_INpcState
+    {
+        public void Enter();
+        public void Update();
+        public void Exit();
+
+
+        public void OnHoverEnter();
+        public void OnHoverExit();
+        public void OnCatchPressed();
+        public void OnCatchReleased();
+    }
+}
