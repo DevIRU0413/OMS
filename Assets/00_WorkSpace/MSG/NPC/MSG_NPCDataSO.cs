c9bc380dd53baa47ae72f340e4691fa92787beb2
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 02:01:30 +0900

    [feat]: 개발용 NPC 데이터 SO 생성
    - CSV 파싱 기능이 개발되면 순수 C# 클래스를 활용하여 데이터를 주입할 예정이지만 현재 개발 전 상태이기에 SO를 통해 데이터 주입하기 위해 생성
    - 또는 디버그용 및 테스트용으로 사용

---
+++ Assets/00_WorkSpace/MSG/NPC/MSG_NPCDataSO.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public enum MSG_CharType
+    {
+        Capturable,
+        Rival,
+        Boss,
+        Disturb
+    }
+
+    public enum MSG_CatchableNPCtype
+    {
+        None,
+        Handsome,
+        Normal,
+        Ugly,
+        Special
+    }
+
+    /// <summary>
+    /// CSV 파싱 이전 개발용 NPC 데이터 모델입니다.
+    /// </summary>
+    [CreateAssetMenu(fileName = "MSG_NPCDataSO", menuName = "ScriptableObjects/MSG_NPCDataSO")]
+    public class MSG_NPCDataSO : ScriptableObject
+    {
+        public          int                     ID;
+        public          string                  Name;
+
+        public          MSG_CharType            CharType;
+        public          MSG_CatchableNPCtype    CatchableNPCtype;
+
+        public          float                   CharAttackDamage;
+
+        public          float                   CharMaxHealth;
+        public          float                   CurrentCharHealth;
+
+        public          float                   CharMaxCatchGauge;
+        public          float                   CurrentCharCatchGauge;
+
+        public          float                   CharCatchGaugeHealValue;
+
+        public          float                   CharWalkSpeed;
+        public          float                   CharRunSpeed;
+
+        public          int                     CurrentScore;
+        public          int                     CatchScore;
+        public          int                     ClickScore;
+        public          int                     FollowScore;
+
+
+        public void Init()
+        {
+            CurrentCharHealth = CharMaxHealth;
+            CurrentCharCatchGauge = 0;
+            CurrentScore = 0;
+        }
+    }
+}
