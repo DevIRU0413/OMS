9aa6b4e3565dca2bb1a41c9a5c9a10efdb50aee4
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 02:07:00 +0900

    [feat]: 포획 가능 NPC의 포획 중 상태 추가
    - 마우스를 누르고 있을 때 포획 게이지가 올라가는 로직 구현
    - 잠깐의 시간 이후 주변 라이벌을 찾고 있다면 경쟁 상태로 전환

---
+++ Assets/00_WorkSpace/MSG/NPC/State Machine/MSG_CatchingState.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class MSG_CatchingState : MSG_INpcState
+    {
+        private MSG_CatchableNPC _npc;
+
+        private Coroutine _waitForCheckRivalCO;
+
+        public MSG_CatchingState(MSG_CatchableNPC npc)
+        {
+            _npc = npc;
+        }
+
+
+        public void Enter()
+        {
+            _npc.StartCaptureGauge();
+
+            if (_waitForCheckRivalCO != null)
+            {
+                _npc.StopCoroutine(_waitForCheckRivalCO);
+            }
+            _waitForCheckRivalCO = _npc.StartCoroutine(WaitAndCheckRival());
+        }
+
+        public void Update()
+        {
+            _npc.IncreaseGauge(_npc.Settings.CaptureGaugeIncreasePerSecond * Time.deltaTime);
+
+            if (_npc.IsGaugeFull())
+            {
+                _npc.ChangeState(new MSG_CapturedState(_npc));
+            }
+        }
+
+        public void Exit()
+        {
+            _npc.StopCaptureGauge();
+        }
+
+        public void OnCatchReleased()
+        {
+            _npc.ChangeState(new MSG_AimedState(_npc));
+        }
+
+        private IEnumerator WaitAndCheckRival()
+        {
+            yield return new WaitForSeconds(_npc.Settings.StartDetectionDelay); // 시작 지연 시간 후에 경쟁자 확인
+            if (_npc.HasNearbyRival())
+            {
+                _npc.ChangeState(new MSG_CompetingState(_npc)); // 경쟁 상태로 전환
+            }
+        }
+
+        #region Unused Methods
+        public void OnCatchPressed() { }
+        public void OnHoverEnter() { }
+        public void OnHoverExit() { }
+        #endregion
+    }
+
+}
