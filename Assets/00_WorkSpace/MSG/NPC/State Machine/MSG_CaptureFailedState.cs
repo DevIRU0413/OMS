17337bd950d0c47de321637e8795773943cced79
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 02:09:25 +0900

    [feat]: 포획 가능 NPC의 포획 실패 상태 추가
    - 현재 어떤 로직이 구현되어야 될 지 미정이기에 별개의 로직은 구현하지 않음

---
+++ Assets/00_WorkSpace/MSG/NPC/State Machine/MSG_CaptureFailedState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class MSG_CaptureFailedState : MSG_INpcState
+    {
+        private MSG_CatchableNPC _npc;
+
+        public MSG_CaptureFailedState(MSG_CatchableNPC npc)
+        {
+            _npc = npc;
+        }
+
+        public void Enter()
+        {
+            _npc.PlayFailEffect();
+            _npc.DisableInteraction();
+        }
+        public void Update()
+        {
+            // 사라지는 로직?
+        }
+
+        #region Unused Methods
+        public void Exit() { }
+        public void OnHoverEnter() { }
+        public void OnHoverExit() { }
+        public void OnCatchPressed() { }
+        public void OnCatchReleased() { }
+        #endregion
+    }
+}
