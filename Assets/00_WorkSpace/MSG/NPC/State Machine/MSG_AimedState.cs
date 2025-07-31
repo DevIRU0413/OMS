0e241cdf713d14dee3e36e99a78b1b6afc1b97a4
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 02:06:08 +0900

    [feat]: 포획 가능 NPC의 조준된 상태 추가
    - 배회하는 로직은 같으나 조준된 상태인 UI를 표시하기 위한 상태 추가

---
+++ Assets/00_WorkSpace/MSG/NPC/State Machine/MSG_AimedState.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+namespace MSG
+{
+    public class MSG_AimedState : MSG_INpcState
+    {
+        private MSG_CatchableNPC _npc;
+
+        private float _stateTime;
+        private float _currentTime;
+        private bool _isMoving;
+        private Vector2 _moveDirection;
+
+        public MSG_AimedState(MSG_CatchableNPC npc)
+        {
+            _npc = npc;
+        }
+
+        public void Enter()
+        {
+            _npc.ShowAimUI(true);
+        }
+
+        public void Update()
+        {
+            _currentTime += Time.deltaTime;
+
+            if (_currentTime >= _stateTime)
+            {
+                SetNewWanderState();
+            }
+
+            if (_isMoving)
+            {
+                _npc.transform.Translate(_moveDirection * Time.deltaTime * _npc.NPCData.CharWalkSpeed);
+            }
+        }
+
+        public void Exit()
+        {
+            _npc.ShowAimUI(false);
+        }
+
+
+        public void OnHoverExit() => _npc.ChangeState(new MSG_WanderingState(_npc));
+        public void OnCatchPressed() => _npc.ChangeState(new MSG_CatchingState(_npc));
+
+
+        private void SetNewWanderState()
+        {
+            _isMoving = Random.value > _npc.Settings.MoveProbability; // MoveProbability * 100 % 확률로 이동 결정
+            _stateTime = Random.Range(_npc.Settings.MinDuration, _npc.Settings.MaxDuration); // MinDuration ~ MaxDuration 사이의 행동 시간 설정
+            _currentTime = 0f;
+
+            if (_isMoving)
+            {
+                _moveDirection = Random.Range(0, 2) == 0 ? Vector2.right : Vector2.left; // 좌우로 랜덤하게 이동
+
+                if (_moveDirection == Vector2.left)
+                {
+                    _npc.FlipX(true);
+                }
+            }
+            else
+            {
+                _moveDirection = Vector2.zero;
+            }
+        }
+
+        #region Unused Methods
+        public void OnHoverEnter() { }
+        public void OnCatchReleased() { }
+        #endregion
+    }
+}
