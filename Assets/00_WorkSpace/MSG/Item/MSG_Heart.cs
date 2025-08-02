9778467c1ca62de73976fd1501a1251fbcd17bef
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-03 04:08:47 +0900

    [feat]: 체력 회복용 하트 로직 추가

---
+++ Assets/00_WorkSpace/MSG/Item/MSG_Heart.cs
@@ -0,0 +1,26 @@
+﻿using Core.UnityUtil.PoolTool;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class MSG_Heart : MonoBehaviour, MSG_ICollectible
+    {
+        [SerializeField] private MSG_HeartData _data;
+
+        public void Collect()
+        {
+            MSG_PlayerLogic logic = MSG_PlayerReferenceProvider.Instance.GetPlayerLogic();
+            logic.Heal(_data.HealAmout);
+            Vanish();
+        }
+
+        private void Vanish()
+        {
+            // 사라지는 애니메이션 필요 시 넣기
+            PoolManager.Instance.Despawn(gameObject);
+        }
+    }
+}
