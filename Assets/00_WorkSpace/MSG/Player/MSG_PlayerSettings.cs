487a8c42903bdbfc019d370e3fa2a425b89fa6b9
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-03 04:25:46 +0900

    [feat]: 플레이어 설정값 조절용 SO 추가
    - 피격 후 무적 시간 조절용 필드 추가

---
+++ Assets/00_WorkSpace/MSG/Player/MSG_PlayerSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    [CreateAssetMenu(fileName = "MSG_PlayerSettings", menuName = "ScriptableObjects/MSG_PlayerSettings")]
+    public class MSG_PlayerSettings : ScriptableObject
+    {
+        public float InvincibleTime; // 무적시간
+
+    }
+}
