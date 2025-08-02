f28907910755b9cfa96616b5d841032efe27f18a
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-03 04:15:30 +0900

    [feat]: 풀링 사용으로 인해 SO를 복제 생성하여 사용하도록 기능 추가

--- Assets/00_WorkSpace/MSG/NPC/MSG_NPCBase.cs
+++ Assets/00_WorkSpace/MSG/NPC/MSG_NPCBase.cs
@@ -8,15 +8,27 @@ namespace MSG
     public abstract class MSG_NPCBase : MonoBehaviour
     {
         [SerializeField] protected MSG_NPCDataSO _npcData;
+        [SerializeField] protected MSG_NPCMoveController _moveController;
         [SerializeField] protected Animator _animator;
         [SerializeField] protected CapsuleCollider2D _collider;
         [SerializeField] protected SpriteRenderer _spriteRenderer;
 
+
+        public MSG_NPCMoveController NPCMoveController => _moveController;
         // CSV 파싱 기능 개발 전 NPC 작동을 위한 SO 주입
         public MSG_NPCDataSO NPCData => _npcData;
 
+        protected virtual void Awake()
+        {
+            if (_moveController == null)
+            {
+                _moveController = GetComponent<MSG_NPCMoveController>();
+            }
+        }
+
         protected virtual void OnEnable()
         {
+            _npcData = Instantiate(_npcData); // 새로 생성해서 사용
             Init();
         }
 
