b7cfd635349a4ccdb19e9d956e3ae3ed4879f95e
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-03 04:17:53 +0900

    [feat]: 방해 NPC 등록 로직 추가
    - 플레이어가 방해 NPC와 Trigger 시 GetComponent를 우회하기 위해 방해 NPC도 추가

--- Assets/00_WorkSpace/MSG/NPC/MSG_NPCProvider.cs
+++ Assets/00_WorkSpace/MSG/NPC/MSG_NPCProvider.cs
@@ -9,18 +9,17 @@ namespace MSG
     {
         private static Dictionary<Collider2D, MSG_RivalNPC> _rivalNpcMap = new();
         private static Dictionary<Collider2D, MSG_CatchableNPC> _catchableNpcMap = new();
+        private static Dictionary<Collider2D, MSG_DisturbNPC> _disturbNpcMap = new();
 
 
         public static void RegisterRival(MSG_RivalNPC rival, CapsuleCollider2D col)
         {
             _rivalNpcMap[col] = rival;
         }
-
         public static void UnregisterRival(MSG_RivalNPC rival, CapsuleCollider2D col)
         {
             _rivalNpcMap.Remove(col);
         }
-
         public static bool TryGetRival(CapsuleCollider2D col, out MSG_RivalNPC npc)
         {
             return _rivalNpcMap.TryGetValue(col, out npc);
@@ -40,10 +39,25 @@ namespace MSG
             return _catchableNpcMap.TryGetValue(col, out npc);
         }
 
+        public static void RegisterDisturb(MSG_DisturbNPC disturb, Collider2D col)
+        {
+            _disturbNpcMap[col] = disturb;
+        }
+        public static void UnregisterDisturb(MSG_DisturbNPC disturb, Collider2D col)
+        {
+            _disturbNpcMap.Remove(col);
+        }
+        public static bool TryGetDisturb(Collider2D col, out MSG_DisturbNPC npc)
+        {
+            return _disturbNpcMap.TryGetValue(col, out npc);
+        }
+
+
         public static void Clear()
         {
             _rivalNpcMap.Clear();
             _catchableNpcMap.Clear();
+            _disturbNpcMap.Clear();
         }
     }
 }
