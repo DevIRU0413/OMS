474b6b8309a1a33d38e4760d9cfcc3f5f33d927c
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-03 04:25:23 +0900

    [feat]: 플레이어 기능 다수 추가
    - 방해 NPC와 Trigger시 피격 기능 추가
    - 피격 시 넘어짐 기능 추가
    - 1회 피격 무시용 빵 봉투 기능 추가
    - 하트 획득 시 체력 회복 기능 추가

--- Assets/00_WorkSpace/MSG/Player/MSG_PlayerLogic.cs
+++ Assets/00_WorkSpace/MSG/Player/MSG_PlayerLogic.cs
@@ -8,42 +8,143 @@ namespace MSG
 {
     public class MSG_PlayerLogic : MonoBehaviour
     {
+        #region Fields, Properties, Actions
         [SerializeField] private MSG_PlayerData _playerData;
         [SerializeField] private Animator _animator;
         [SerializeField] private CapsuleCollider2D _capsuleCollider2D;
         [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private LayerMask _disturbNPCLayer;
+        [SerializeField] private LayerMask _heartLayer;
+        [SerializeField] private LayerMask _breadBoxLayer;
+        [SerializeField] private MSG_PlayerSettings _playerSettings;
+        [SerializeField] private GameObject _breadBag;
 
         private bool _isWornBreadBag = false;
         private bool _isFever = false;
+        private Coroutine _invincibleCO;
+        private bool _isFallen = false;
 
         public MSG_PlayerData PlayerData => _playerData;
         public SpriteRenderer PlayerSpriteRenderer => _spriteRenderer;
         public bool IsFever => _isFever;
+        public bool IsWornBreadBag => _isWornBreadBag;
 
         public event Action OnPlayerFeverStarted;
         public event Action OnPlayerFeverEnded;
         public event Action OnPlayerDied;
-
+        #endregion
 
         private void Awake()
+        {
+            //_playerData.Init();
+        }
+
+        private void Start()
         {
             _playerData.Init();
         }
 
-        private void Update()
-        {
-            LowerHPByTime();
-        }
-
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            // 아이템 처리
-            // 장애물 NPC 부딪힘 처리
+            if (((1 << collision.gameObject.layer) & _disturbNPCLayer) != 0)
+            {
+                MSG_NPCProvider.TryGetDisturb(collision, out MSG_DisturbNPC npc);
+                TryFallDown(npc.NPCData.CharAttackDamage);
+            }
+
+            // TODO: 이것도 GetComponent 우회를 위해 Provider 처럼 쓰는 것 고려
+            if (((1 << collision.gameObject.layer) & _heartLayer) != 0)
+            {
+                var collectible = collision.GetComponent<MSG_ICollectible>();
+                if (collectible != null)
+                {
+                    collectible.Collect();
+                }
+            }
+
+            if (((1 << collision.gameObject.layer) & _breadBoxLayer) != 0)
+            {
+                var collectible = collision.GetComponent<MSG_ICollectible>();
+                if (collectible != null)
+                {
+                    collectible.Collect();
+                }
+            }
         }
 
-        private void LowerHPByTime()
+        public void Heal(int amount)
         {
+            _playerData.CurrentHP = Mathf.Min(_playerData.CurrentHP + amount, MSG_PlayerData.MaxHP);
+            Debug.Log($"치료됨{_playerData.CurrentHP}");
+        }
 
+        public void TryWearBreadBag()
+        {
+            // 빵 봉투 쓰는 로직
+            _isWornBreadBag = true;
+            _breadBag.SetActive(true);
+        }
+
+        private void TryFallDown(float damage)
+        {
+            // 1. 빵 봉투 착용 중이라면
+            if (_isWornBreadBag)
+            {
+                // 빵 봉투 소모
+                _isWornBreadBag = false;
+                _breadBag.SetActive(false);
+
+                // 무적 처리
+                if (_invincibleCO != null)
+                {
+                    StopCoroutine(_invincibleCO);
+                }
+                _invincibleCO = StartCoroutine(InvincibleRoutine());
+
+                return;
+            }
+
+            // 2. 이미 넘어진 상태라면 무시
+            if (_isFallen) return;
+
+            // 3. 넘어짐 처리
+            _isFallen = true;
+            TakeDamage(damage);
+
+            if (_invincibleCO != null)
+            {
+                StopCoroutine(_invincibleCO);
+            }
+            _invincibleCO = StartCoroutine(InvincibleRoutine());
+        }
+
+        private IEnumerator InvincibleRoutine()
+        {
+            Debug.Log("무적 시간 시작");
+
+            _capsuleCollider2D.enabled = false;
+            yield return new WaitForSeconds(_playerSettings.InvincibleTime);
+            _capsuleCollider2D.enabled = true;
+            _isFallen = false;
+
+            Debug.Log("무적 시간 끝");
+        }
+
+        private void TakeDamage(float amount)
+        {
+            _playerData.CurrentHP -= (int)amount; // 소수점 버려짐, 의도하지 않은거라면 바꿔야 될 듯
+
+            if (_playerData.CurrentHP <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            // 죽음 처리
+            OnPlayerDied?.Invoke();
+            Debug.Log("죽음!");
         }
     }
 }
