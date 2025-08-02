6166340edcadd0eac23626ab0ce43df24dbf7a67
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-03 04:28:11 +0900

    [feat]: 풀링 이전 MVP 패턴용 UIInstaller, UIPresenter
    - 현재는 사용하지 않지만 NPC 풀링을 사용하기 이전 사용하던 UIInstaller, UIPresenter 추가

---
+++ Assets/00_WorkSpace/MSG/Temp UI/Deprecated_MSG_UIInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    //public class MSG_UIInstaller : MonoBehaviour
+    //{
+    //    [SerializeField] private MSG_CatchGauge _catchGaugeView;
+    //    [SerializeField] private MSG_CompeteGauge _competeGaugeView;
+    //    [SerializeField] private MSG_HPView _hpView;
+    //    [SerializeField] private MSG_CatchableNPC _catchableNPCModel;
+
+    //    private MSG_UIPresenter _uiPresenter;
+
+    //    private void Awake()
+    //    {
+    //        _uiPresenter = new MSG_UIPresenter(_catchGaugeView, _competeGaugeView, _hpView, _catchableNPCModel);
+    //    }
+    //}
+}
