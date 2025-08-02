784085e532e5183e884543faef8580ed9ea92b1e
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-03 04:08:32 +0900

    [feat]: 빵 봉투 로직 추가

---
+++ Assets/00_WorkSpace/MSG/Item/MSG_BreadBag.cs
@@ -0,0 +1,31 @@
+﻿using Core.UnityUtil.PoolTool;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class MSG_BreadBag : MonoBehaviour, MSG_ICollectible
+    {
+        public void Collect()
+        {
+            MSG_PlayerLogic logic = MSG_PlayerReferenceProvider.Instance.GetPlayerLogic();
+
+            if (logic.IsWornBreadBag)
+            {
+                Debug.Log("이미 빵 봉투를 쓰고 있어 return");
+                return;
+            }
+
+            logic.TryWearBreadBag();
+            Vanish();
+        }
+
+        private void Vanish()
+        {
+            // 사라지는 애니메이션 필요 시 넣기
+            PoolManager.Instance.Despawn(gameObject);
+        }
+    }
+}
