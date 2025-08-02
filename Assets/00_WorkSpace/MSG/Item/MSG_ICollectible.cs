2711cd2cddfd9978017e16d2d74ea016540e35c0
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-03 04:08:15 +0900

    [feat]: 획득 가능한 행동 정의용 ICollectible 인터페이스 추가

---
+++ Assets/00_WorkSpace/MSG/Item/MSG_ICollectible.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public interface MSG_ICollectible
+    {
+        public void Collect();
+    }
+}
