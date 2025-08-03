699b8f270e8c609ff4ed9bb840693e30dbba9bb3
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-04 01:50:38 +0900

    [feat]: 포획된 NPC의 따라가는 로직용 클래스 생성
    - 아직 구조만 생각해두고 미구현 상태

---
+++ Assets/00_WorkSpace/MSG/NPC/MSG_FollowController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class MSG_FollowController : MonoBehaviour
+    {
+        private Transform _targetTransform; // 플레이어 또는 앞의 NPC Transform
+        private bool _isAttatched = false;  // 바로 붙는 것이 아닌 붙는 애니메이션이 있어야 자연스러울 것 같음
+
+
+        private void Update()
+        {
+            if (!_isAttatched) return;
+
+            Move();
+        }
+
+
+        private void Attatch()
+        {
+            // 붙기 시작하는 Lerp 등의 애니메이션
+
+            _isAttatched = true;
+        }
+
+        private void Move()
+        {
+            
+        }
+    }
+}
