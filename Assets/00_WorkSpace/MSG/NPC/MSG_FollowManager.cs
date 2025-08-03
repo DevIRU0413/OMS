ecf8a33d8970330acbc8889478ab01979104628a
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-04 01:51:32 +0900

    [feat]: FollowController를 관리하는 매니저 생성
    - 아직 구조만 생각해두고 미구현 상태

---
+++ Assets/00_WorkSpace/MSG/NPC/MSG_FollowManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class MSG_FollowManager : MonoBehaviour
+    {
+        [SerializeField] private Transform _playerTransform;
+        private List<MSG_CatchableNPC> _captured = new();
+
+        public void AddCapturedNPC(MSG_CatchableNPC npc)
+        {
+            // CatchableNPC에서 MSG_FollowManager 오브젝트를 직렬화해서 갖고 있기
+            // 포획 시 CatchableNPC에서 MSG_FollowManager.AddCapturedNPC에 자기 자신을 넘기기
+            // MSG_CatchableNPC 오브젝트의 직렬화된 MSG_FollowController 인스턴스를 Public 프로퍼티로 열어두기
+            // 그럼 MSG_CatchableNPC의 MSG_FollowController를 참조하여 Transform을 배정해서 넘겨주기
+            // 이후 FollowController에서 Attatch 이후 게임이 끝날 때까지 따라다니기
+        }
+    }
+}
