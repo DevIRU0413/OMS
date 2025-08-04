e07096b4b65df2bec640bb751392d26ae9a9826e
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-05 00:37:21 +0900

    [feat]: NPC 추종을 관리하는 매니저 추가

--- Assets/00_WorkSpace/MSG/NPC/MSG_FollowManager.cs
+++ Assets/00_WorkSpace/MSG/NPC/MSG_FollowManager.cs
@@ -7,16 +7,52 @@ namespace MSG
 {
     public class MSG_FollowManager : MonoBehaviour
     {
-        [SerializeField] private Transform _playerTransform;
+        [SerializeField] private MSG_MouseCursorManager _mouseCursorManager;
+        
+        private Transform _playerTransform;
         private List<MSG_CatchableNPC> _captured = new();
+        public static MSG_FollowManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            _mouseCursorManager.OnDirectionChanged += BroadcastDirectionChanged;
+            _playerTransform = MSG_PlayerReferenceProvider.Instance.GetPlayerTransform();
+        }
+
+        private void OnDestroy()
+        {
+            if (_mouseCursorManager != null)
+            {
+                _mouseCursorManager.OnDirectionChanged -= BroadcastDirectionChanged;
+            }
+        }
 
         public void AddCapturedNPC(MSG_CatchableNPC npc)
         {
-            // CatchableNPC에서 MSG_FollowManager 오브젝트를 직렬화해서 갖고 있기
-            // 포획 시 CatchableNPC에서 MSG_FollowManager.AddCapturedNPC에 자기 자신을 넘기기
-            // MSG_CatchableNPC 오브젝트의 직렬화된 MSG_FollowController 인스턴스를 Public 프로퍼티로 열어두기
-            // 그럼 MSG_CatchableNPC의 MSG_FollowController를 참조하여 Transform을 배정해서 넘겨주기
-            // 이후 FollowController에서 Attatch 이후 게임이 끝날 때까지 따라다니기
+            Transform followTarget = _captured.Count == 0 ? _playerTransform : _captured[^1].transform;
+
+            npc.FollowController.Init(followTarget, _captured.Count, _mouseCursorManager.MoveDir);
+            _captured.Add(npc);
+        }
+
+        private void BroadcastDirectionChanged()
+        {
+            float moveDir = _mouseCursorManager.MoveDir;
+
+            for (int i = 0; i < _captured.Count; i++)
+            {
+                _captured[i].FollowController.OnDirectionChanged(moveDir);
+            }
         }
     }
 }
