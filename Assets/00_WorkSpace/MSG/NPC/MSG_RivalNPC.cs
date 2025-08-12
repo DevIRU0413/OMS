b4dfff2c6caa34c25f9c81ee57ca10f137777543
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-12 21:30:54 +0900

    [feat]: 라이벌 NPC 기능 다수 추가
    - 경쟁 실패 시 날아가는 애니메이션 추가
    - 디스폰 로직 추가
    - 경쟁 중 정지 로직 추가
    - 추후 바라보는 로직 추가 예정

--- Assets/00_WorkSpace/MSG/NPC/MSG_RivalNPC.cs
+++ Assets/00_WorkSpace/MSG/NPC/MSG_RivalNPC.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+
+using Core.UnityUtil.PoolTool;
+
 using UnityEngine;
 
 
@@ -7,19 +10,88 @@ namespace MSG
 {
     public class MSG_RivalNPC : MSG_NPCBase
     {
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+        private bool _isCompeting = false;
+        private Coroutine _flyingCO;
+
         private void Start()
         {
             MSG_NPCProvider.RegisterRival(this, _collider);
         }
 
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            if (_flyingCO != null)
+            {
+                StopCoroutine(_flyingCO);
+                _flyingCO = null;
+            }
+            _isCompeting = false;
+        }
+
         private void Update()
         {
+            if (_isCompeting) return; // 경쟁 중에는 이동 중지
             _moveController.Tick();
         }
 
         private void OnDestroy()
         {
             MSG_NPCProvider.UnregisterRival(this, _collider);
+        }
+
+
+        public void StartCompeting()
+        {
+            _isCompeting = true;
+            // TODO: 바라보는 로직 추가
+        }
+
+        public void EndCompeting()
+        {
+            _isCompeting = false;
+        }
+
+        // 패배 후 사라져야됨
+        public void LoseAndDespawn()
+        {
+            if (_flyingCO != null)
+            {
+                StopCoroutine(_flyingCO);
+                _flyingCO = null;
+            }
+            _flyingCO = StartCoroutine(FlyingRoutine());
+        }
+
+        // 날아가는 애니메이션
+        private IEnumerator FlyingRoutine()
+        {
+            // TODO: 매직 넘버 전부 setting으로 옮기기
+
+            _moveController.StopMovement();
+
+            _collider.enabled = false;
+            _rigidbody2D.gravityScale = _settings.GravityScale;
+            _rigidbody2D.constraints = RigidbodyConstraints2D.None;
+
+            float angle = Random.Range(_settings.MinAngle, _settings.MaxAngle);
+            float rad = angle * Mathf.Deg2Rad;
+            Vector2 dir = new Vector2(Mathf.Sin(rad), Mathf.Cos(rad)); // 0도가 위를 바라봄
+
+            float impulse = _settings.ImpulsePower;
+            _rigidbody2D.AddForce(dir * impulse, ForceMode2D.Impulse);
+
+            // 회전 필요시 넣기
+
+            yield return new WaitForSeconds(_settings.DespawnTime);
+
+            _collider.enabled = true;
+            _rigidbody2D.gravityScale = 0f;
+            _rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+
+            PoolManager.Instance.Despawn(this.gameObject);
         }
     }
 }
