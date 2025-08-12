66169f70de4ae1fbd33998608d75a46605b76c95
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-12 21:29:10 +0900

    [feat]: MoveController 기능 다수 추가
    - 맵 끝에 도달 시 강제 이동 로직 추가
    - 라이벌 NPC 한정 피격 발동 시 바라보는 방향으로 이동 로직 추가

--- Assets/00_WorkSpace/MSG/NPC/MSG_NPCMoveController.cs
+++ Assets/00_WorkSpace/MSG/NPC/MSG_NPCMoveController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 namespace MSG
@@ -10,11 +12,21 @@ namespace MSG
         [SerializeField] private MSG_NPCSettings _settings;
         [SerializeField] private SpriteRenderer _spriteRenderer;
         [SerializeField] private MSG_NPCDataSO _dataSO;
+        [SerializeField] private MSG_NPCBase _npcBase;
 
         private float _stateTime;
         private float _currentTime;
         private bool _isMoving;
         private Vector2 _moveDirection;
+        private int _forceMoveCount;
+        private Direction _endReachedDirection;
+        private bool _isStopped = false;
+
+        // 강제 이동 관련
+        private MSG_PlayerLogic _playerLogic;
+        private bool _isForcedMove;
+        private float _forcedMoveEndTime;
+        private Vector2 _forcedDir;
 
 
         private void Awake()
@@ -30,9 +42,50 @@ namespace MSG
             }
         }
 
+        private void OnEnable()
+        {
+            _isStopped = false;
+        }
+
+        private void Start()
+        {
+            _playerLogic = MSG_PlayerReferenceProvider.Instance.GetPlayerLogic();
+            if (_playerLogic != null)
+            {
+                _playerLogic.OnPlayerDamaged += MoveWhenTriggered;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_playerLogic != null)
+            {
+                _playerLogic.OnPlayerDamaged -= MoveWhenTriggered;
+            }
+        }
+
+
         public void Tick()
         {
+            if (_isStopped) return; // 라이벌 패배 시 이동 정지 후 날아가는 애니메이션을 위한 플래그
+
             _currentTime += Time.deltaTime;
+
+            // 피격 시 강제 이동 우선 처리
+            if (_isForcedMove)
+            {
+                float speed = _dataSO.CharWalkSpeed * _settings.ForcedMoveSpeedMultiplier;
+                transform.Translate(_forcedDir * Time.deltaTime * speed);
+
+                if (Time.time >= _forcedMoveEndTime)
+                {
+                    _isForcedMove = false;
+                    // 강제 이동이 끝나면 자연스럽게 다음 배회 상태 재추첨
+                    _stateTime = 0f;
+                    _currentTime = _stateTime; // 바로 SetNewWanderState() 타게
+                }
+                return; // 배회 로직 건너뜀
+            }
 
             if (_currentTime >= _stateTime)
             {
@@ -44,6 +97,20 @@ namespace MSG
                 transform.Translate(_moveDirection * Time.deltaTime * _dataSO.CharWalkSpeed);
             }
         }
+        public void ReachEnd(Direction endReachedDirection)
+        {
+            if (_isForcedMove) return; // 강제 이동 중엔 반전 강제 무시, 속도가 빠르면 맵을 많이 벗어나는 것 처럼 보일 수 있음
+
+            _forceMoveCount = _npcBase.Settings.ForceMoveCount;
+            _endReachedDirection = endReachedDirection;
+        }
+
+        // 라이벌 NPC 날아가는 애니메이션 재생용 이동 중지 플래그
+        public void StopMovement()
+        {
+            _isStopped = true;
+        }
+
 
         private void SetNewWanderState()
         {
@@ -53,17 +120,38 @@ namespace MSG
 
             if (_isMoving)
             {
-                _moveDirection = Random.Range(0, 2) == 0 ? Vector2.right : Vector2.left; // 좌우로 랜덤하게 이동
-
-                if (_moveDirection == Vector2.left)
+                if (_forceMoveCount > 0)
                 {
-                    _spriteRenderer.flipX = _moveDirection == Vector2.left;
+                    _moveDirection = _endReachedDirection == Direction.Left ? Vector2.right : Vector2.left; // 끝에 다달은 반대 방향으로 강제로 이동하도록 설정
+                    _forceMoveCount--;
                 }
+                else
+                {
+                    _moveDirection = Random.Range(0, 2) == 0 ? Vector2.right : Vector2.left; // 좌우로 랜덤하게 이동
+                }
+
+                _spriteRenderer.flipX = _moveDirection == Vector2.left;
             }
             else
             {
                 _moveDirection = Vector2.zero;
             }
+        }
+
+        // 플레이어와 피격 시 무한 피격을 막기 위해 강제 이동
+        // 요구 사항은 바라보는 방향으로 쭉 가는 것
+        private void MoveWhenTriggered()
+        {
+            if (_npcBase is not MSG_DisturbNPC) return; // 방해 NPC에서만 동작
+
+            _forcedDir = _spriteRenderer.flipX ? Vector2.left : Vector2.right; // 현재 NPC가 바라보는 방향 설정
+
+            _isForcedMove = true;
+            _forcedMoveEndTime = Time.time + _settings.ForcedMoveDuration;
+
+            // 배회 상태와 충돌하지 않게 즉시 상태 리셋하여 다음 프레임에 새 상태 뽑도록 처리
+            _isMoving = true;
+            _stateTime = 0f;
         }
     }
 }
