821930b46f1d09391d481b5407609929c0c8b591
DevIRU <deviru0413@gmail.com>
2025-08-13 16:39:09 +0900

    [feat]: 채팅 매니저 기능 추가

---
+++ Assets/00_WorkSpace/YSJ/Scripts/YSJ_ChattingManager.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class YSJ_ChattingManager : YSJ_SimpleSingleton<YSJ_ChattingManager>, IManager
+{
+    [SerializeField] private bool _isChattingEnabled = true;
+
+    [SerializeField] private float _chattingUpdateMinTime = 1.0f;
+    [SerializeField] private float _chattingUpdateMaxTime = 2.0f;
+
+    [SerializeField, Range(1, 30)] private int _maxChattingCount = 10;
+
+    private Queue<string> _chattingMessages;
+
+    public Action<string[]> OnChattingMessageAdded;
+
+    public bool IsChattingEnabled => _isChattingEnabled;
+    public bool IsDontDestroy => isDontDestroyOnLoad;
+    public int MaxChattingCount => _maxChattingCount;
+
+    #region IManager
+    public void Initialize() => SetChattingEnabled();
+    public void Cleanup() => ClearChattingMessages();
+
+    public GameObject GetGameObject() => this.gameObject;
+
+    #endregion
+
+    private void SetChattingEnabled()
+    {
+        if (_isChattingEnabled)
+        {
+            _chattingMessages = new Queue<string>(_maxChattingCount);
+            Debug.Log("Chatting enabled.");
+        }
+        else
+        {
+            ClearChattingMessages();
+            Debug.Log("Chatting disabled.");
+        }
+    }
+    private void ClearChattingMessages()
+    {
+        _chattingMessages?.Clear();
+    }
+
+    public void AddChattingMessage(string message)
+    {
+        if (_isChattingEnabled)
+        {
+            _chattingMessages?.Enqueue(message);
+            Debug.Log($"Added chat message: {message}");
+
+            OnChattingMessageAdded?.Invoke(GetChattingMessages());
+        }
+        else
+        {
+            Debug.LogWarning("Chatting is disabled or max count reached.");
+        }
+    }
+
+    public string[] GetChattingMessages()
+    {
+        if (_isChattingEnabled && _chattingMessages != null)
+        {
+            string[] messages = _chattingMessages.ToArray();
+            return messages;
+        }
+        else
+        {
+            Debug.LogWarning("Chatting is disabled, cannot retrieve messages.");
+            return new string[0];
+        }
+    }
+}
