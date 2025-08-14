3a05fde652a0e3c28be7cf7848ef520b3c105c90
DevIRU <deviru0413@gmail.com>
2025-08-14 23:28:31 +0900

    [feat]: Ending Cat SO 추가

---
+++ Assets/00_WorkSpace/YSJ/Scripts/SO/EndingBranchScore.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class EndingBranchScore
+{
+    public SceneID SceneID;
+    public int ScoreCat;
+}
---
+++ Assets/00_WorkSpace/YSJ/Scripts/SO/EndingBranchScoreCatData.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "YSJ_EndingBranchScoreCat", menuName = "ScriptableObjects/YSJ_EndingBranchScoreCat")]
+public class EndingBranchScoreCatData : ScriptableObject
+{
+    public List<EndingBranchScore> endingBranchScores;
+}
