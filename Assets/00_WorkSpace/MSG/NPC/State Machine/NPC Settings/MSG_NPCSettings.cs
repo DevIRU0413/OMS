dae29a530ddd7f4b0c791eecf3093ae54d702563
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-17 05:06:02 +0900

    [feat]: 방해 NPC 스폰 관련 필드 추가

--- Assets/00_WorkSpace/MSG/NPC/State Machine/NPC Settings/MSG_NPCSettings.cs
+++ Assets/00_WorkSpace/MSG/NPC/State Machine/NPC Settings/MSG_NPCSettings.cs
@@ -50,9 +50,11 @@ namespace MSG
         public float NpcDistance = 2f;                          // 포획된 NPC 간의 이격 거리
         public float SmoothSpeed = 5f;                          // 부드럽게 따라오는 속도
         public float AttachSpeed = 2f;                          // 포획 직후 플레이어에게 붙는 속도, 값이 작을수록 빠르게 붙습니다
-        public float BobAmplitude = 0.5f;                      // 위 아래로 얼마나 움직일 지 거리 설정
-        public float BobDuration = 1f;                         // 한 번 이동하는데 걸리는 시간
+        public float BobAmplitude = 0.5f;                       // 위 아래로 얼마나 움직일 지 거리 설정
+        public float BobDuration = 1f;                          // 한 번 이동하는데 걸리는 시간
 
+        [Header("Disturb NPC 스폰 관련 설정")]
+        public float[] SpawnTimes = new float[2] { 60f, 80f };  // Disturb NPC가 스폰될 시간의 배열. 코드 예시로는 60초, 80초에 한 번 스폰됩니다.
         //[Header("포획 완료 설정")]
         //public float CaptureDistance = 1.5f;                    // 포획 완료 시 NPC 간의 거리
     }
